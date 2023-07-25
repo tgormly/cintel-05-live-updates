aa834412115fefedbff3638f525320cfc1f691b9
Tim Gormly <89597142+tgormly@users.noreply.github.com>
2023-07-24 21:36:05 -0500

    updated theme, name, and links

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ async def update_csv_files():
         await asyncio.sleep(60)  # wait for 60 seconds
 
 app_ui = ui.page_navbar(
-    shinyswatch.theme.lumen(),
+    shinyswatch.theme.morph(),
     ui.nav(
         "MT_Cars",
         ui.layout_sidebar(
@@ -46,14 +46,14 @@ app_ui = ui.page_navbar(
             get_mtcars_outputs(),
         ),
     ),
-    ui.nav(ui.a("About", href="https://github.com/denisecase")),
-    ui.nav(ui.a("GitHub", href="https://github.com/denisecase/cintel-05-live-updates")),
-    ui.nav(ui.a("App", href="https://denisecase.shinyapps.io/cintel-05-live-updates/")),
+    ui.nav(ui.a("About", href="https://github.com/tgormly")),
+    ui.nav(ui.a("GitHub", href="https://github.com/tgormly/cintel-05-live-updates")),
+    ui.nav(ui.a("App", href="https://tgormly.shinyapps.io/cintel-05-live-updates/")),
     ui.nav(ui.a("Plotly Express", href="https://plotly.com/python/line-and-scatter/")),
     ui.nav(ui.a("WeatherAPI", href="https://openweathermap.org/api")),
     ui.nav(ui.a("OneCallAPI", href="https://openweathermap.org/api/one-call-3")),
     ui.nav(ui.a("File_Reader", href="https://shiny.rstudio.com/py/api/reactive.file_reader.html")),
-    title=ui.h1("Case Dashboard"),
+    title=ui.h1("Tim Gormly Dashboard"),
 )
 
 
