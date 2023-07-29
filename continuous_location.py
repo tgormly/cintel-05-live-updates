616c62a2f33d3deef8f27927d39b08cfbbe92a8a
Tim Gormly <89597142+tgormly@users.noreply.github.com>
2023-07-29 17:59:02 -0500

    Files and logs updated

--- continuous_location.py
+++ continuous_location.py
@@ -86,7 +86,7 @@ async def get_temperature_from_openweathermap(lat, long):
     logger.info(f"Calling fetch_from_url for {open_weather_url}")
     result = await fetch_from_url(open_weather_url, "json")
     logger.info(f"Data from openweathermap: {result}")
-    temp_F = data["main"]["temp"]
+    temp_F = result.data["main"]["temp"]
     # temp_F = randint(68, 77)
     return temp_F
 
