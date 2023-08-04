5bc1fa3a5483a597b3ad73344e456de4d9d1e9f1
Tim Gormly <89597142+tgormly@users.noreply.github.com>
2023-08-03 20:04:27 -0500

    Updated number of updates held to scale with list

--- continuous_location.py
+++ continuous_location.py
@@ -106,7 +106,7 @@ async def update_csv_location():
         locations = ["ELY MN", "Death Valley CA", "Maryville MO", "Madrid, Spain"]
         update_interval = 60  # Update every 1 minute (60 seconds)
         total_runtime = 15 * 60  # Total runtime maximum of 15 minutes
-        num_updates = 10  # Keep the most recent 10 readings
+        num_updates = 10 * len(locations) # Keep the most recent 10 readings
         logger.info(f"update_interval: {update_interval}")
         logger.info(f"total_runtime: {total_runtime}")
         logger.info(f"num_updates: {num_updates}")
